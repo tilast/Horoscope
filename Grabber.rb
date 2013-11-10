5c8e9345ff92532a42c25e212e5f32800e809b3b
tilast <tilast.lab@gmail.com>
2013-11-10 14:30:20 +0200

    upd4 fixed grabbing

--- Grabber.rb
+++ Grabber.rb
@@ -3,7 +3,7 @@ require "json"
 require 'data_mapper'
 require 'dm-sqlite-adapter'
 require 'dm-migrations'
-require "./Horoscope.rb"
+require "./models/Horoscope.rb"
 require "date"
 
 Dir.mkdir 'tmp' unless File.exists? 'tmp'
