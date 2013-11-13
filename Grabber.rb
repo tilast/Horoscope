5f759e6b8d06c699d004f6f918f1271f505f079e
tilast <tilast.lab@gmail.com>
2013-11-14 00:32:43 +0200

    fixed grabbing date

--- Grabber.rb
+++ Grabber.rb
@@ -20,13 +20,12 @@ class Grabber
     end
     def simple_grab
       now = DateTime.now
-      today  = Date.new(now.year, now.month, now.day - 1)
 
       horoscope = Horoscope.new
-      horoscope[:date] = today
+      horoscope[:date] = Date.new(now.year, now.month, now.day)
 
       Horoscope::SIGNS.each do |sign|
-        resp = Net::HTTP.get_response(URI.parse(URI.escape("http://widgets.fabulously40.com/horoscope.json?sign=" + sign + "&date=" + today.strftime("%Y-%m-%d"))))
+        resp = Net::HTTP.get_response(URI.parse(URI.escape("http://widgets.fabulously40.com/horoscope.json?sign=" + sign + "&date=" + Date.new(now.year, now.month, now.day - 1).strftime("%Y-%m-%d"))))
         result = JSON.parse(resp.body)
         horoscope[sign.to_sym] = result.empty? ? "Sorry, today we don't have horoscope for you :(" : result["horoscope"]["horoscope"]
       end
